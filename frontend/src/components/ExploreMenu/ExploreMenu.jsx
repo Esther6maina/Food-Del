1b63bf534df5320634ca52a808d659ecbf365a7d
Esther Maina <estanyambura99@gmail.com>
2025-02-13 19:27:49 +0300

    Updated menu selection logic and styling

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import './ExploreMenu'
+import './ExploreMenu.css'
 import { menu_list } from '../../assets/assets'
 
-const ExploreMenu = () => {
+const ExploreMenu = ({ category, setCategory }) => {
+
   return (
     <div className='explore-menu' id='explore-menu'>
       <h1>Explore Our Menu</h1>
-      <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes with the finest ingredients and culinary expertise.Our mission is to satisfy  your cravings and elevent your dining experience, our delicious meal at a time</p>
-      <div className='explore-menu-list'>
+      <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes.Our mission is to satisfy  your cravings and elevent your dining experience, our delicious meal at a time</p>
+      <div className="explore-menu-list">
         {menu_list.map((item,index)=>{
            return (
-             <div key={index} className='explore-menu-list-item'>
-               <image src={item.menu_image} alt=""/>
+             <div onClick={() =>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className='explore-menu-list-item'>
+               <img className={category===item.menu_name?"active":""} src={item.menu_image} alt=""/>
                <p>{item.menu_name}</p>
 
              </div>
@@ -19,6 +20,7 @@ const ExploreMenu = () => {
         })}
 
       </div>
+      <hr />
       </div>
   )
 }
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css'
 const Header = () => {
   return (
     <div className='header'>
-      <div class="header-contents">
+      <div className="header-contents">
         <h2>Order your<br/> favourite food here</h2>
         <p>Choose from a diverse menu featuring a delectable array of dishes with the finest ingredients and culinary expertise.Our mission is to satisfy  your cravings and elevent your dining experience, our delicious meal at a time</p>
         <button>View Menu</button>
